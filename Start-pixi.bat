0094c68da513990d8d0b0c5a08830ff04c4ee6e3
patwuch <patwuch@github.com>
2026-04-02 12:46:46 +0800

    remove old launch files

--- Start-pixi.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0pixi-powershell-script.ps1"
-if %ERRORLEVEL% neq 0 (
-    echo.
-    echo  Something went wrong. See above for details.
-    pause
-)
